2fe97e678e1a033ec362f556793253764359d44e
kanashimia <56224949+kanashimia@users.noreply.github.com>
2020-02-10 00:05:50 +0200

    Update Hilbert.hs

--- FunctionalHilbert/src/Hilbert.hs
+++ FunctionalHilbert/src/Hilbert.hs
@@ -4,11 +4,9 @@ import Diagrams.Prelude
 import Data.Colour.RGBSpace
 import Data.Colour.RGBSpace.HSV
 
-
 type Fractal = Trail V2 Double
 type DiagramFractal b = Renderable (Path V2 Double) b
                      => QDiagram b V2 Double Any
-
 
 hilbert :: Int -> Fractal
 hilbert 0 = mempty
@@ -16,7 +14,6 @@ hilbert n = hilbert (n-1)  # rotateBy (1/4) # reflectY <> vrule 1
          <> hilbert (n-1) <> hrule 1
          <> hilbert (n-1) <> vrule (-1)
          <> hilbert (n-1)  # rotateBy (1/4) # reflectX
-
 
 peano :: Int -> Fractal
 peano 0 = mempty
@@ -27,7 +24,6 @@ peano n = strip <> hrule 1
     strip = peano (n-1) <> vrule 1
          <> peano (n-1)  # reflectX <> vrule 1
          <> peano (n-1)
-
 
 strokeFractal :: Located Fractal -> DiagramFractal b
 strokeFractal = mconcat . colourize . explodeTrail
