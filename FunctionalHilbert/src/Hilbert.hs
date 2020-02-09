1bdcfe437a895f1f71b5d024a99d038480d85e56
kanashimia <56224949+kanashimia@users.noreply.github.com>
2020-02-09 09:39:15 +0200

    Update Hilbert.hs

--- FunctionalHilbert/src/Hilbert.hs
+++ FunctionalHilbert/src/Hilbert.hs
@@ -4,7 +4,6 @@ import Diagrams.Prelude
 import Data.Colour.RGBSpace
 import Data.Colour.RGBSpace.HSV
 
-
 hilbert :: Int -> Trail V2 Double
 hilbert 0 = mempty
 hilbert n = hilbert (n-1)  # rotateBy (1/4) # reflectY <> vrule 1
@@ -15,13 +14,9 @@ hilbert n = hilbert (n-1)  # rotateBy (1/4) # reflectY <> vrule 1
 
 drawHilbert n = hilbert n # strokeT # lc white
 
-
-drawHilbertColor n
-    = mconcat
-    $ zipWith lc =<< colors . length
-    $ map (lineCap LineCapRound . strokeLocT)
-    $ explodeTrail
-    $ hilbert n `at` origin
+drawHilbertColor n = (hilbert n `at` origin)
+    # explodeTrail # map (lineCap LineCapRound . strokeLocT)
+    # (zipWith lc =<< colors . length) # mconcat
   where
     colors n = cycle $ map makeColor [0,360 / realToFrac n..360]
     makeColor a = uncurryRGB sRGB $ hsv a 1 1
